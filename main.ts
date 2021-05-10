c4fb3dc573edcead447e23dddeb26b87806b37a1
oscher-ocampo <83966218+oscher-ocampo@users.noreply.github.com>
2021-05-10 10:50:53 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    conteo_conductividad += 1
+    sumatoria_conductividad = sumatoria_conductividad + proporcionalidad
+    promedio_conectividad = sumatoria_conductividad / conteo_conductividad
+    basic.showNumber(promedio_conectividad)
+})
 let conductividad = 0
 let proporcionalidad = 0
+let promedio_conectividad = 0
+let sumatoria_conductividad = 0
+let conteo_conductividad = 0
+conteo_conductividad = 0
+sumatoria_conductividad = 0
+promedio_conectividad = 0
 basic.forever(function () {
     conductividad = pins.analogReadPin(AnalogPin.P2)
     proporcionalidad = pins.map(
@@ -9,5 +21,4 @@ basic.forever(function () {
     0,
     100
     )
-    basic.showNumber(proporcionalidad)
 })
